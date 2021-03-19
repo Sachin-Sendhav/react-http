c83f29fd0c78cbdd3c3acbc4ef87219af3bde304
Sachin-Sendhav <sacchu1714@gmail.com>
2021-03-20 01:30:41 +0530

    post request update

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class PostForm extends Component {
 
@@ -13,15 +14,29 @@ class PostForm extends Component {
         }
     }
 
-    changeHandler() {
-console.log('hmm chALEGGGAAAaa')
+    changeHandler = e => {
+        this.setState({ [e.target.name]: e.target.value })
     }
+
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('https://jsonplaceholder.typicode.com/posts', this.state)
+        .then(response =>{
+            console.log(response)
+        })
+        .catch(error  =>{
+            console.log(error)
+        })
+    }
+
+
 
     render() {
         const { userId, title, body } = this.state
         return (
             <div>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     <div>
                         <input type="text" name="userId" value={userId} onChange={this.changeHandler} />
                     </div>
@@ -31,6 +46,7 @@ console.log('hmm chALEGGGAAAaa')
                     <div>
                         <input type="text" name="body" value={body} onChange={this.changeHandler} />
                     </div>
+                    <button type="submit">submit</button>
 
                 </form>
             </div>
